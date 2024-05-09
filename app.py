34fab65bd8fb28ccab215c944831257b587584f4
Ray Chu <raytanaka8@gmail.com>
2024-05-08 21:08:00 -0700

    changes to html pages and css with main branch

--- app.py
+++ app.py
@@ -30,5 +30,25 @@ def read_url():
 def login():
     return render_template("login.html")
 
+@app.route("/faq")
+def faq():
+    return render_template("faq.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/privacy")
+def privacy():
+    return render_template("privacy.html")
+
+@app.route("/terms")
+def terms():
+    return render_template("terms.html")
+
 if __name__ == "__main__":
     app.run(debug = True, host = "localhost", port = 8008)
