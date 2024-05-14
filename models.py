ff0611353a4391bd040ba13aed6ceb5c90bbfee3
Ngai Lam Chou <ngai.lam.chou@gmail.com>
2024-05-13 22:01:58 -0700

    Create New Record in Video Implemented

--- models.py
+++ models.py
@@ -1,25 +1,26 @@
-from sqlalchemy import Numeric, ForeignKey, Integer, String, Text
-from sqlalchemy.orm import mapped_column, relationship
+from sqlalchemy import Column, ForeignKey, Integer, String, Text
+from sqlalchemy.orm import relationship
 from database import db
 from flask_login import UserMixin
-from datetime import datetime
 
 class User(db.Model, UserMixin):
-    id = mapped_column(Integer, primary_key=True)
-    username = mapped_column(String(100), unique=True, nullable=False)
-    password = mapped_column(String(255), nullable=False)
-    videos = db.relationship('Video', back_populates='user', lazy=True)
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(100), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+    videos = relationship('Video', back_populates='user', lazy=True)
 
 class Video(db.Model):
-    id = mapped_column(Integer, primary_key=True)
-    title = mapped_column(String(255), nullable=False)
-    url = mapped_column(String(255), nullable=False)
-    user_id = mapped_column(Integer, ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', back_populates='videos')
-    comments = db.relationship('Comment', back_populates='video', lazy=True)
+    __tablename__ = 'video'
 
-class Comment(db.Model):
-    id = mapped_column(Integer, primary_key=True)
-    text = mapped_column(Text, nullable=False)
-    video_id = mapped_column(Integer, ForeignKey('video.id'), nullable=False)
-    video = db.relationship('Video', back_populates='comments')
+    id = Column(Integer, primary_key=True)
+    title = Column(String(255), nullable=False)
+    url = Column(String(255), nullable=False)
+    views = Column(Integer, nullable=False)
+    likes = Column(Integer, nullable=False)
+    dislikes = Column(Integer, nullable=False)
+    word = Column(Text, nullable=False)
+    date = Column(String(10), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user = relationship('User', back_populates='videos')
