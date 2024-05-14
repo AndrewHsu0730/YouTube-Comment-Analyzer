83f48b34b567874a20b1e37797e1ff0c265834ad
Andrew Hsu <andrew311000gmail.com>
2024-05-14 10:55:16 -0700

    Add a little bit of tests

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+from app import app
+
+@pytest.fixture(scope="module")
+def client():
+    app_for_testing = app()
+    client = app_for_testing.test_client()
+    yield client
--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,6 +1,15 @@
+import pytest
 from models import User
 
 def test_new_user():
+    # with pytest.raises(BaseException):
+    #     user = User(username="Ash")
+
+    # with pytest.raises(BaseException):
+    #     user = User(password="thestressedguy")
+
+    user = User(username="Ash")
+    assert user is not None
+
     user = User(username="Ash", password="thestressedguy")
-    assert user.username == "Ash"
-    # assert user.password == "thestressedguy"
+    assert user is not None
