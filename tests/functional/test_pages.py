3bef63d33429140df929537da57d34047b59ad8c
Andrew Hsu <andrew311000gmail.com>
2024-05-15 09:58:09 -0700

    Create 1 functional test that fails

--- tests/functional/test_pages.py
+++ tests/functional/test_pages.py
@@ -1,17 +1,18 @@
-# from conftest import create_app
-# from flask_login import LoginManager
-# from models import User
+from conftest import create_app
+from flask_login import LoginManager
+from models import User
 
-# app = create_app()
+app = create_app()
 
-# login_manager = LoginManager()
-# login_manager.login_view = "authorization.home"
-# login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.login_view = "authorization.home"
+login_manager.init_app(app)
 
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User.query.get(int(user_id))
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-# with app.test_client() as test_client:
-#     response = test_client.get("/")
-#     assert response.status_code == 200
+with app.test_client() as test_client:
+    def test_landing_page():
+        response = test_client.get("localhost:8008/")
+        assert response.status_code == 200
