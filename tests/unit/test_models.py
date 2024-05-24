36f0dce0fa97c98aaa6f3721e4c738929fd3b500
Andrew Hsu <andrew311000gmail.com>
2024-05-24 03:30:06 -0700

    Change hashing algorithm in unit tests

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -18,7 +18,7 @@ def test_new_user():
             db.session.add(user)
             db.session.commit()
 
-        user = User(username="admin", password=generate_password_hash("admin123", method="sha512"))
+        user = User(username="admin", password=generate_password_hash("admin123", method="scrypt"))
         db.session.add(user)
         db.session.commit()
         assert user is not None
